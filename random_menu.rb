82838d4a1a23e4af06a6ad813f43dc2ca900c65a
Zheng Cao <chori84@gmail.com>
2018-02-05 15:50:16 -0800

    Finish first enhancement

--- random_menu.rb
+++ random_menu.rb
@@ -3,16 +3,23 @@ taste_array = ["sweet", "sour", "salty", "hot", "creamy", "bitter", "umami", "bu
 food_array = ["chicken", "beef", "cheeze", "apple", "salad", "broccoli", "fish", "burrito", "steak", "tofu"]
 message_array = []
 
-def random_number
+def random_number(max)
   r = Random.new
-  index = r.rand(0...10)
+  index = r.rand(0...max)
+end
+
+def delete_current_item(array, index)
+  array.delete_at(index)
 end
 
 10.times { |i|
-  index_method = random_number
-  index_taste = random_number
-  index_food = random_number
+  index_method = random_number(10 - i)
+  index_taste = random_number(10 - i)
+  index_food = random_number (10 - i)
   message_array[i] = "#{ i + 1 }. " + method_array[index_method] + " " + taste_array[index_taste] + " " + food_array[index_food]
+  delete_current_item(method_array, index_method)
+  delete_current_item(taste_array, index_taste)
+  delete_current_item(food_array, index_food)
 }
 
 puts message_array
