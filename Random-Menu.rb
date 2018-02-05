37c3b55645250425c38a111a855e69d17b56658c
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-05 15:23:11 -0800

    Starting on day 1 homework

---
+++ Random-Menu.rb
@@ -0,0 +1,4 @@
+puts "Here is a list of our wonderful menu items."
+fd_adj = [ "Spicy Hot" , "Cold" , "Bland" , "Exciting" , "Extreme" , "Deadly" , "Lovely" , "Chilled" , "Boiling"]
+fd_style = [ "fried" , "baked" , "seared" , "steamed" , "stinky" , "broiled" , "roasted" , "grilled" , "sauted" , "frozen"]
+fd_name = ["ice cream" , "soup" , "chicken" , "crab" , "beef" , "cabbage" , "potato" , "carrots" , "salad" , "lobster"]
