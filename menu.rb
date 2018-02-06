ab200e7a5f4e129b9165c0b706a0d9a0411ff0d3
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-06 11:56:50 -0800

    Fix min array length bug and errors in comments

--- menu.rb
+++ menu.rb
@@ -27,7 +27,7 @@ end
 
 # Until the user provides a number greater than or equal to one and less than
 # provided max (if max is not nil), prompts the user for a number for provided
-# items_type. Returns the number.
+# items_type items. Returns the number.
 def get_number(items_type, max)
   loop do
     print "\nHow many #{items_type} would you like to have? "
@@ -40,8 +40,8 @@ def get_number(items_type, max)
 end
 
 # Takes in user-provided item for provided items_type. Returns the item if the
-# it contains at least one character and is not already an item in provided
-# input_items.
+# it contains at least one, non-empty character and is not already an item
+# in provided input_items.
 def get_user_item_if_valid(items_type, input_items)
   possible_item = gets.chomp.strip.gsub(/\s+/, " ")
   if possible_item.empty?
@@ -53,28 +53,29 @@ def get_user_item_if_valid(items_type, input_items)
   end
 end # returns 'nil' if user provides empty String or a duplicate value
 
-# Prints message about
+# Prints prompt about needing provided num amount of provided items_type items.
 def print_prompt(num, items_type)
   print "Please enter #{num} unique "
   # removes 's' to make grammatically correct if only one of items_type
   num > 1 ? (puts "#{items_type}.") : (puts "#{items_type.delete_suffix("s")}.")
 end
 
-# Prompts user for items for items_type type until the user has entered provided
-# num_of_items items. Returns the list.
+# Prompts user for items of provided items_type type until the user has entered
+# provided num_of_items items. Returns the list.
 def get_custom_list(num_of_items, items_type)
   input_items = [] # stores user-provided items for items_type
   (1..num_of_items).each do |item_num|
     print "  #{item_num}. "
     user_item = get_user_item_if_valid(items_type, input_items)
-    # user_item is 'nil' if user provided empty String or a duplicate value
+    # user_item is 'nil' if user entered an empty String or a duplicate item
     user_item.nil? ? redo : input_items.push(user_item)
   end
   return input_items
 end
 
-# Prompts user for the amount of provided items_type items and each item.
-# Returns a list of provided items_type items.
+# Prompts user for the number of provided items_type items the user wants to
+# enter. Until the user enters the user-prescribed number of items, prompts the
+# user for items. Returns the items.
 def get_custom(items_type)
   num_of_items = get_number(items_type, nil) # 'nil' for no max
   print_prompt(num_of_items, items_type)
@@ -101,7 +102,7 @@ end
 # 'descriptions', 'cooking methods', or 'foods'; otherwise returns 'nil'.
 #
 # If provided default_or_custom is 'default', returns the default values for
-# provided items_type. Otherwise, prompts user to enter item of items_type
+# provided items_type. Otherwise, prompts user to enter items of items_type
 # and returns the user-provided items.
 def get_options(default_or_custom, items_type)
   default_or_custom == "default" ? (return get_default(items_type)) :
@@ -111,7 +112,8 @@ end
 # Using the descriptions, cooking methods, and foods lists from the provided
 # options_type list, prints the number of menu items as dictated by the user.
 def print_menu(options_types)
-  num_of_items = get_number("menu items", options_types.min.length)
+  min_length = options_types.collect { |item| item.length }.min
+  num_of_items = get_number("menu items", min_length)
   puts "\nHere's your menu:"
   (1..num_of_items).each do |item_num|
     print "  #{item_num}."
@@ -122,7 +124,6 @@ end
 
 # ------------------------------------------------------------------------- main
 print_intro
-
 default_or_custom = get_default_or_custom
 
 descriptions = get_options(default_or_custom, "descriptions")
