9d2aa8d9015e519068837cd1ec6228ad762653f8
Zheng Cao <chori84@gmail.com>
2018-02-05 16:54:05 -0800

    Add comments

--- random_menu.rb
+++ random_menu.rb
@@ -1,9 +1,11 @@
+# Create variables for data storage
 method_array = []
 taste_array = []
 food_array = []
 message_array = []
-number_of_item = 11
+number_of_item = 0
 
+# Helper method to create arrays based on user's inputs
 def create_arrays(array)
   item = gets.chomp.downcase
   until item == "quit"
@@ -13,7 +15,7 @@ def create_arrays(array)
   end
 end
 
-
+# Instruction for data inputs
 puts "Welcome to Random Menu! Let's create your own food database now."
 puts "Please enter some cooking methods. Enter 'quit' when you are done."
 create_arrays(method_array)
@@ -22,9 +24,11 @@ create_arrays(taste_array)
 puts "Please enter some of your favorite food. Enter 'quit' when you are done."
 create_arrays(food_array)
 
+# Pick the max number of items that user can choose
 array_sizes = [method_array.size, taste_array.size, food_array.size]
 max_number_of_item = array_sizes.min
 
+# Ask for number of items from user
 puts "How many items do you want to check out? Pick a number between 0 and #{max_number_of_item}."
 number_of_item = gets.chomp.to_i
 while number_of_item > max_number_of_item
@@ -32,15 +36,18 @@ while number_of_item > max_number_of_item
   number_of_item = gets.chomp.to_i
 end
 
+# Random number generator
 def random_number(max)
   r = Random.new
   index = r.rand(0...max)
 end
 
+# Delete chosen item from arrays
 def delete_current_item(array, index)
   array.delete_at(index)
 end
 
+# Create output message
 number_of_item.times { |i|
   index_method = random_number(number_of_item - i)
   index_taste = random_number(number_of_item - i)
@@ -51,5 +58,6 @@ number_of_item.times { |i|
   delete_current_item(food_array, index_food)
 }
 
+# Display output message
 puts "Here is your list:"
 puts message_array
